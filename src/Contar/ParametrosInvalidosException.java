695f856105e2e2722442d4c484706a3f5390d934
kevin <kevin.aparecido@gmail.com>
2024-01-26 19:05:57 -0300

    kevin

---
+++ src/Contar/ParametrosInvalidosException.java
@@ -0,0 +1,13 @@
+package Contar;
+
+public class ParametrosInvalidosException extends Throwable {
+
+
+
+    public ParametrosInvalidosException() {
+
+
+        super("O segundo parâmetro deve ser maior que o primeiro");
+
+    }
+}
